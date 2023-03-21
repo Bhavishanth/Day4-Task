817488a6f137cf3e91547164171b2e915bcdde02
Bhavishanth Suresh <bhavishanth@gmail.com>
2023-03-21 11:27:33 +0530

    Add files via upload

---
+++ httpRequest_flag.js
@@ -0,0 +1,18 @@
+var xhr = new XMLHttpRequest;
+
+xhr.open('GET', 'https://restcountries.com/v3.1/all')
+xhr.responseType = 'json';
+
+xhr.send();
+
+xhr.onload = function()
+{  
+    var responseObj = xhr.response;
+    console.log(responseObj);
+
+    for(var i = 0;i < responseObj.length; i++)
+    {
+        console.log(responseObj[i].flag)
+    }
+
+}
