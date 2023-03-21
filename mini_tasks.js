f174ec76f513f45e03f212449a3c7fe99a01afc3
Bhavishanth Suresh <bhavishanth@gmail.com>
2023-03-21 16:10:21 +0530

    Add files via upload

---
+++ mini_tasks.js
@@ -0,0 +1,61 @@
+// Declare four variables without assigning values and print them in console
+var a;
+var b;
+var c;
+var d;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// How to get value of the variable myvar as output
+
+var myvar= 1;
+console.log("myvar");
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let first_name = "Bhavishanth";
+let last_name = "Suresh"
+let marital_status = "Single";
+let Country = "India";
+let Age = "24"
+
+console.log(first_name, last_name, marital_status, Country, Age);
+
+// Declare variables to store your first name, last name, marital status, country and age in a multiple line
+
+console.log(first_name);
+console.log(last_name);
+console.log(marital_status);
+console.log(Age)
+
+// Declare variables and assign string, boolean, undefined and null data types
+
+let A = "apple";
+let B = true;
+let C = null;
+let D = undefined;
+
+// Convert the string to integer
+
+let num = "32";
+let num1 = parseInt(num);
+let num2 = +num;
+let num3 = Number("32");
+
+// //Square of a number
+let value = prompt("Give me a number");
+let n4 = parseInt(value);
+console.log(n4*n4);
+
+//Swapping 2 numbers
+
+let Z1 = 23
+let Z2 = 34
+ let swap = Z1
+ Z1 = Z2;
+ Z2 = swap;
+
+ console.log("the newer value of Z1",Z1)
+ console.log("the newer value of Z2",Z2);
